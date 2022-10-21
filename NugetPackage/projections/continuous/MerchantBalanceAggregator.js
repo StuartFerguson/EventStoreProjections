a191e93416896de6dd94d6fac88ba4dad2840bc1
Stuart Ferguson <stuart_ferguson1development@outlook.com>
2022-10-21 17:50:19 +0100

    Added new projection

---
+++ NugetPackage/projections/continuous/MerchantBalanceAggregator.js
@@ -0,0 +1,46 @@
+//starttestsetup
+var fromAll = fromAll || require("../../../EventStoreProjections/node_modules/@transactionprocessing/esprojection-testing-framework").scope.fromAll;
+var linkTo = linkTo || require("../../../EventStoreProjections/node_modules/@transactionprocessing/esprojection-testing-framework").scope.linkTo;
+//endtestsetup
+
+isValidEvent = function (e) {
+
+    if (e) {
+        if (e.data) {
+            if (e.isJson) {
+                if (e.eventType !== "$metadata") {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+};
+
+getMerchantId = function (e) {
+    if (e.data.merchantId === undefined) {
+        return null;
+    }
+    return e.data.merchantId;
+};
+
+fromAll()
+    .when({
+        $any: function (s, e) {
+            if (isValidEvent(e)) {
+                var merchantId = getMerchantId(e);
+                if (merchantId !== null) {
+                    if (e.eventType == "MerchantCreatedEvent" ||
+                        e.eventType === "ManualDepositMadeEvent" ||
+                        e.eventType === "AutomaticDepositMadeEvent" ||
+                        e.eventType === "TransactionHasStartedEvent" ||
+                        e.eventType === "TransactionHasBeenCompletedEvent" ||
+                        e.eventType === "MerchantFeeAddedToTransactionEvent") {
+                        var streamName = "MerchantBalanceArchive-" + merchantId.replace(/-/gi, "");
+                        linkTo(streamName, e);
+                    }
+                }
+            }
+        }
+    });
